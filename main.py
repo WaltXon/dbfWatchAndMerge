39e1d53fb31374ce0e99cb72b8b03cfb2a958143
Walt <walt@waltnixon.com>
2013-06-10 16:46:38 -0500

    added variable for windows

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ import watch
 import placePnts
 import os
 
-target_dir = os.path.join("/","users", "waltnixon", "Documents", "github", "dbfWatchAndMerge", "rigdata")
+# target_dir = os.path.join("/","users", "waltnixon", "Documents", "github", "dbfWatchAndMerge", "rigdata")
+target_dir = "C:\Users\wnixon\Documents\GitHub\dbfWatchAndMerge"
 
 print ("target_dir = {0}".format(target_dir))
 
