9b8dc2970b239f13f9ce03805b88a0125696b171
Walt <walt@waltnixon.com>
2013-06-11 17:38:08 -0500

    removeDups

--- dbfer.py
+++ dbfer.py
@@ -1,5 +1,7 @@
 import dbf
 import os
+import datetime
+import csv
 
 def getAndFilterFiles(path, filt, fileFormat):
 	'''Get and Filter a list of files type = filt Files'''
@@ -25,16 +27,21 @@ def mergeDbfFiles(files):
 	# mergeTable.open()
 	
 	i=0 #Counter for header
+	catTable = []
 	for aDbf in files:
 		print("currently on table = {0}".format(aDbf))
-		catTable = []
+
 		table = dbf.Table(aDbf)
 		table.open()
 
 		fields = dbf.get_fields(aDbf)
+		fields.append("date_added")
 		print("fields = {0}".format(fields))
+		dateAdded = datetime.date.fromtimestamp(os.path.getctime(aDbf))
+		print ("date_added = {0}".format(dateAdded))
+		print("No records in table = {0}".format(len(table)))
 
-		for rec in table[:10]:
+		for rec in table:
 			tmp = []
 			if i == 0:
 				catTable.append(fields)
@@ -42,19 +49,55 @@ def mergeDbfFiles(files):
 			for item in rec:
 				tmp.append(item)
 			# print ("row added = {0}".format(tmp))
-		catTable.append(tmp)
+			tmp.append(dateAdded)
+			catTable.append(tmp)
 
 		table.close()
-	print catTable, fields
+		print("Length of catTable = {0}".format(len(catTable)))
+	# print (catTable[:2])
+	return (catTable, fields)
 
 def removeDups(inputList, fields):
-
 	clean = []
 	print fields
 	wellIdInx = fields.index('well_id')
-	
-
-
+	print ("wellIdInx = {0}".format(wellIdInx))
+	dateInx = fields.index("date_added")
+	print ("dateInx = {0}".format(dateInx))
+	wellIds = []
+	print ("inputListLenght = {0}".format(len(inputList)))
 	for row in inputList:
-		temp = []
+		wellIds.append(row[wellIdInx])
+	uniqWellIds = set(wellIds)
+	print uniqWellIds
+
+	for wellId in uniqWellIds:
+		dups = []
+		
+		for row2 in inputList:
+			if wellId == row2[wellIdInx]:
+				print("wellId = {0} : row2[wellIdInx] = {1}".format(wellId, row2[wellIdInx]))
+				dups.append(row2)
+
+		if len(dups) > 1:
+			newest = datetime.date.min
+			for item in dups:
+				if dups[dateInx] > newest:
+					newest = dups[dateInx]
+					tmp = item
+			clean.append(tmp)
+		else: 
+			clean.append(dups)
+
+
+	print clean[:10]
+	print ("Clean length = {0}".format(len(clean)))
+	return clean
+
+
+def writeCSV(inList, outFile):
+	with open(outFile, 'wb') as f:
+		writer = csv.writer(f)
+		writer.writerows(inList)
+	return outFile
 
--- main.py
+++ main.py
@@ -3,13 +3,15 @@ import watch
 import placePnts
 import os
 
-target_dir = os.path.join("/","users", "waltnixon", "Documents", "github", "dbfWatchAndMerge", "rigdata")
-# target_dir = "C:\Users\wnixon\Documents\GitHub\dbfWatchAndMerge"
-
+# target_dir = os.path.join("/","users", "waltnixon", "Documents", "github", "dbfWatchAndMerge", "rigdata")
+target_dir = r"C:\Users\wnixon\Documents\GitHub\dbfWatchAndMerge\rigdata"
+out_csv = r"C:\Users\wnixon\Documents\GitHub\dbfWatchAndMerge\rigdata\out_csv.csv"
 print ("target_dir = {0}".format(target_dir))
 
 dbfFiles = dbfer.getAndFilterFiles(target_dir, "l", "dbf")
 
-mergeFile, fields = dbfer.mergeDbfFiles(dbfFiles)
+result = dbfer.mergeDbfFiles(dbfFiles)
 
-removeDups(mergeFile, fields)
+clean = dbfer.removeDups(result[0], result[1])
+
+dbfer.writeCSV(clean, out_csv)
